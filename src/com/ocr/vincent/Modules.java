8211a6eec4cd2eef98aeabcb551b2018eaec82e8
Vincent Favre <vincent.gw16@gmail.com>
2019-10-09 18:04:23 +0200

    Mode Duel [OK] Boucle replay avec control de saisie

--- src/com/ocr/vincent/Modules.java
+++ src/com/ocr/vincent/Modules.java
@@ -8,14 +8,14 @@ public class Modules {
     int i; /** compteur boucles*/
     int combLen = 4; /** nbr de chiffres de la combinaison par défaut à charger depuis les paramètres xml */
 
-    int selectedMode; /** Choix du mode de jeu  1 ou 2 ou 3 */
     boolean devMode = false; /** false Normal - true Mode Développeur*/
     boolean helpMode = false; /** false Normal - true Mode Aide mémoire*/
     int nbTry = 0; /** Compteur d'essais */
-    int nbTryLimit = 10; /** nombre d'essais autorisés avant de trouver la bonne solution*/
+    int nbTryLimit = 4; /** nombre d'essais autorisés avant de trouver la bonne solution*/
     int victory = 0; /** 0 combinaison pas trouvée - 1 combinaison trouvée */
 
-    String winReply= ""; /** Résultalt de la combinaison à obtenir (=) x combLen */
+    String winReply= ""; /** Résultat de la combinaison à obtenir (=) x combLen */
+
     List<Integer> minValues = new ArrayList<Integer>(combLen); /** Mémorise les valeurs min*/
     List<Integer> maxValues = new ArrayList<Integer>(combLen); /** Mémorise les valeurs max*/
     List<Integer> theValues = new ArrayList<Integer>(combLen); /** Mémorise les valeurs justes*/
@@ -24,6 +24,9 @@ public class Modules {
      * LANCEMENT DU JEU
      */
     public void runGame() {
+        boolean rPlay ;
+        int selectedMode;
+        do {
         /** Caclul des variables*/
         for (i=0; i<combLen; i++) {
             winReply =  winReply + "=";
@@ -32,8 +35,9 @@ public class Modules {
             minValues.add(1);
         }
         /** Choix du mode */
-        this.displayModesMenu();
+        selectedMode = this.displayModesMenu();
         /** Lancement du mode de jeu choisi */
+
         switch (selectedMode) {
             case 1: /** MODE CHALLENGER */
                 this.runChallengerMode(); break;
@@ -43,6 +47,8 @@ public class Modules {
                 this.runDualMode(); break;
             default: break;
         }
+        rPlay = this.replayRequest();
+        } while (rPlay);
     }
 
     /**
@@ -52,12 +58,18 @@ public class Modules {
         String genCombi = this.generateCombination(); // combinaison générée aléatoirement
         String askCombi = "";  // Suggestion saisie par l'utilisateur
         String reply=""; // Résultat après comparaison (+ - =)
+        String joueur ="USER";
         do {
             nbTry += 1;
             askCombi = this.askCombination();
-            this.displayCombination(genCombi, askCombi);
-            reply = this.compareCombination(genCombi, askCombi);
-            this.displayResult(reply);
+            this.displayCombination(genCombi, askCombi, joueur);
+            reply = this.compareCombination(genCombi, askCombi, joueur);
+            this.displayResult(reply, joueur);
+            if (nbTry==nbTryLimit) {
+                victory = 1;
+                System.out.println("[PARTIE PERDUE] Nombre d'essais dépassé (" + nbTryLimit + ")");
+                System.out.println("La combinaison à trouver était : " + genCombi);
+            }
         } while (victory == 0);
     }
 
@@ -68,18 +80,21 @@ public class Modules {
         String askCombi = this.askCombination();  // Suggestion saisie par l'utilisateur
         String genCombi = ""; // combinaison générée aléatoirement
         String reply=""; // Résultat après comparaison (+ - =)
+        String joueur ="CPU";
         do {
             nbTry += 1;
             genCombi = this.findCombination();
-            this.displayCombination(askCombi, genCombi);
-            reply = this.compareCombination(askCombi, genCombi);
-            this.displayResult(reply);
-            if (nbTry==nbTryLimit) {
-                victory = 1;
-            }
+            this.displayCombination(askCombi, genCombi, joueur);
+            reply = this.compareCombination(askCombi, genCombi, joueur);
+            this.displayResult(reply, joueur);
         } while (victory == 0);
     }
 
+
+    /**
+     * Recherche par le CPU d'une combinaison en fonction des suggestions précédentes
+     * @return cpuComb : Suggestion faite par le CPU
+     */
     private String findCombination() {
         String cpuComb="";
         int nb = 0;
@@ -92,7 +107,7 @@ public class Modules {
                     nb = theValues.get(i);
                 } else {
                     if (maxValues.get(i) - minValues.get(i) == 1) {
-                        nb = minValues.get(i) + (int) (Math.random() * maxValues.get(i)) - 1;
+                        nb = minValues.get(i) + (int) (Math.random() * maxValues.get(i)) -1;
                         if (nb >= maxValues.get(i)) {
                             nb = maxValues.get(i);
                             if (devMode) System.out.println("nb as max : " + nb);
@@ -118,39 +133,81 @@ public class Modules {
      * LANCEMENT DU MODE DUEL --> MODE 3
      */
     private void runDualMode() {
+        System.out.println("Création de ma combinaison :");
+        System.out.println("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨");
+        String userCombi = this.askCombination();  // Suggestion saisie par l'utilisateur
+        String cpuCombi = this.generateCombination(); // combinaison générée aléatoirement
+        String userReply=""; // Résultat après comparaison (+ - =)
+        String cpuReply=""; // Résultat après comparaison (+ - =)
+        String joueur=""; // Nom du joueur
+        System.out.println("Joueur CPU a créé sa combinaison :");
+        System.out.println("----------------------------------");
+        // System.out.println(userCombi); // A SUPPRIMER
+        // System.out.println(cpuCombi); // A SUPPRIMER
+        do {
+            nbTry += 1;
+            System.out.println("Saisir votre suggestion :");
+            String userSug = this.askCombination();
+            String cpuSug = this.findCombination();
 
+            if (victory==0) {
+                /** TOUR ORDINATEUR*/
+                System.out.println("----------------------------------");
+                joueur = "CPU";
+                this.displayCombination(userCombi, cpuSug, joueur);
+                cpuReply = this.compareCombination(userCombi, cpuSug, joueur);
+                this.displayResult(cpuReply, joueur);
+            }
+
+            if (victory==0){
+                /** TOUR UTILISATEUR*/
+                System.out.println("----------------------------------");
+                joueur ="USER";
+                this.displayCombination(cpuCombi, userSug, joueur);
+                userReply = this.compareCombination(cpuCombi, userSug, joueur);
+                this.displayResult(userReply, joueur);
+                System.out.println("----------------------------------");
+            }
+
+            /** if (nbTry==nbTryLimit) {
+                victory = 1;
+                System.out.println("[PARTIE PERDUE] Nombre d'essais dépassé (" + nbTryLimit + ")");
+            } */
+        } while (victory == 0);
     }
 
     /**
      * Affichage du choix du mode et control de saisie
      */
-    public void displayModesMenu() {
+    public Integer displayModesMenu() {
+        int mode; /** Choix du mode de jeu  1 ou 2 ou 3 */
         do {
             System.out.println("CHOISIR UN MODE DE JEU : 1 - CHALLENGER, 2 - DÉFENSEUR, 3 - DUEL");
-            selectedMode = sc.nextInt();
-            switch (selectedMode) {
+            mode = sc.nextInt();
+            switch (mode) {
                 case 1:
-                    System.out.println("MODE CHALLENGER");
+                    System.out.println("[MODE CHALLENGER]");
                     break;
                 case 2:
-                    System.out.println("MODE DÉFENSEUR");
+                    System.out.println("[MODE DÉFENSEUR]");
                     break;
                 case 3:
-                    System.out.println("MODE DUEL");
+                    System.out.println("[MODE DUEL]");
                     break;
-                case 123: /** Permet d'activer les mode Développeur */
-                    System.out.println("MODE DÉVELOPPEUR ACTIF");
+                case 123: /** Permet d'activer le mode Développeur */
+                    System.out.println("[MODE DÉVELOPPEUR ACTIF]");
                     devMode=true;
                     break;
-                case 456: /** Permet d'activer les mode Aide mémoire */
-                    System.out.println("MODE AIDE MEMOIRE ACTIF");
+                case 456: /** Permet d'activer le mode Aide mémoire */
+                    System.out.println("[MODE AIDE MEMOIRE ACTIF]");
                     helpMode=true;
                     break;
                 default:
                     System.out.println("Taper 1,2 ou 3");
                     break;
             }
-        } while (selectedMode < 1 || selectedMode > 4);
+        } while (mode < 1 || mode > 4);
+        return mode;
     }
 
     /**
@@ -173,8 +230,8 @@ public class Modules {
     /**
      * Demande de saisir une suggestion de combinaison générée (avec numérotation counter/combLen)
      * + Procédure de controle de saisie : nombre de caractère +
-     * userComb = Combinaison saisie par l'utilisateur
      * charControl : True si le controle doit être effectué, false si le controle n'est plus à faire
+     * @return userComb = Combinaison saisie par l'utilisateur
      */
     public String askCombination() {
         String userComb ="";
@@ -200,7 +257,8 @@ public class Modules {
         boolean b1 = false; // état du controle d'entrée. False par défaut. Renvoie True si une erreur est trouvée
         boolean b2; // variable True/false du test isDigit()
         char temp; // variable temporaire pour la boucle. Stocke caractère par caractère de la chaine testComb
-        for (i=0 ; i < combLen; i++) {
+        //for (i=0 ; i < combLen; i++) {
+        for (i=0 ; i < testComb.length(); i++) {
             temp = testComb.charAt(i);
             b2 = Character.isDigit(temp);
             if (!b2){
@@ -214,30 +272,34 @@ public class Modules {
     /**
      * Affiche la proposition faite avec + le nombre d'essais
      */
-    public void displayCombination(String aaa, String bbb) {
+    public void displayCombination(String aaa, String bbb, String joueur) {
         if (devMode) {
             System.out.println("DEV-MODE   : " + aaa);
             }
-        System.out.println("Essai n°" + nbTry);
+        System.out.println("Essai n°" + nbTry + " [" + joueur + "]");
         System.out.println("Suggestion --> " + bbb);
     }
 
     /**
      * COMPARATEUR DES DEUX COMBINAISONS
+     * @param comb1 : Combinaison à trouver
+     * @param comb2 : Combinaison suggerée
+     * @return theReply : Contient le résultat de la comparaison comb1/comb2 (+, -, =)
      */
-    public String compareCombination(String comb1, String comb2) {
-        String theReply = ""; /** Résultat de la suggestion contenant + - ou = */
+    public String compareCombination(String comb1, String comb2, String player) {
+        String theReply = "";
         int secretComb=0 ;
         int suggestComb=0 ;
         /** Comparaison des valeurs*/
         for ( i = 0 ; i<combLen; i++) {
             secretComb = Character.getNumericValue(comb1.charAt(i));
             suggestComb = Character.getNumericValue(comb2.charAt(i));
-
             if (secretComb == suggestComb) {
-                theValues.set(i, secretComb);
-                maxValues.set(i, 0);
-                minValues.set(i, 0);
+                if (player == "CPU") {
+                    theValues.set(i, secretComb);
+                    maxValues.set(i, 0);
+                    minValues.set(i, 0);
+                }
                 theReply = theReply + "=";
             } else if (secretComb < suggestComb) {
                 maxValues.set(i, suggestComb);
@@ -252,8 +314,9 @@ public class Modules {
 
     /**
      * Affichage du résultat
+     * @param theReply : Contient le résultat de la comparaison comb1/comb2 (+, -, =)
      */
-    public void displayResult(String theReply) {
+    public void displayResult(String theReply, String player) {
         /** Affichage des indications */
         System.out.println("Indication --> " + theReply);
         if (helpMode) {
@@ -261,12 +324,60 @@ public class Modules {
             System.out.println("maxValues : " + maxValues);
             System.out.println("minValues : " + minValues);
         }
-
-        if (theReply.contains(winReply)) {
-            victory=1;
-            System.out.println("******************************************");
-            System.out.println("* GAGNÉ! Combinaison trouvée en " + nbTry + " coups. *");
-            System.out.println("******************************************");
+        if (nbTry<=nbTryLimit) {
+            if (theReply.contains(winReply)) {
+                victory = 1;
+                System.out.println("**********************************************************");
+                System.out.println("   Joueur " + player + " A GAGNÉ! Combinaison trouvée en " + nbTry + " coups.");
+                System.out.println("**********************************************************");
+            }
+        } else {
+            victory = 1;
+            System.out.println("[PARTIE PERDUE] Nombre d'essais dépassé (" + nbTryLimit + ")");
         }
+    }
+
+    /**
+     *
+     * Méthode permettant de relancer une partie ou quitter
+     * @return b1 True -> Rejouer, False = Quitter le jeu
+     */
+
+    public boolean replayRequest () {
+        boolean b1 = true; // rue -> Rejouer, False = Quitter le jeu
+        boolean b2 = false; // Controle de saisie True -> erreur, false -> pas d'erreur
+        int myChoise; // Choix 1 ou 2 pour rejouer ou quitter
+        String myChoiseStr = ""; // Choix au format string pour le control de saisie
+        do {
+            System.out.println("Rejouer une partie ? : Taper : 1 -> oui ou 2 -> non");
+            myChoiseStr = sc.nextLine();
+            b2 = this.inputControl(myChoiseStr); // Controle de saisie : True ; erreur trouvée (char et non int)
+            if (b2) {
+
+            } else {
+                myChoise = Integer.parseInt(myChoiseStr);
+                if (myChoise < 1 || myChoise >2){
+                    b2 = true;
+                }
+                switch (myChoise) {
+                    case 1:
+                        b1 = true;
+                        System.out.println("[NOUVELLE PARTIE]");
+                        /** Réinitialisation des variables initiales */
+                        winReply = "";
+                        nbTry = 0;
+                        victory = 0;
+                        theValues.clear();
+                        maxValues.clear();
+                        minValues.clear();
+                    break;
+                    case 2:
+                        b1 = false;
+                        System.out.println("[FIN DE PARTIE]");
+                    break;
+                }
+            }
+        } while (b2);
+        return b1;
     }
 }
